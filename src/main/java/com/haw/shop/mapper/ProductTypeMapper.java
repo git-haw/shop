e18632c30f3b52e519293bef3fc331d6c938ebf9
haw <haw358linux@163.com>
2020-03-18 14:17:09 +0800

    补充提交

---
+++ src/main/java/com/haw/shop/mapper/ProductTypeMapper.java
@@ -0,0 +1,15 @@
+package com.haw.shop.mapper;
+
+import com.haw.shop.model.ProductType;
+import com.haw.shop.util.BaseDao;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.type.JdbcType;
+
+public interface ProductTypeMapper extends BaseDao<ProductType> {
+}
