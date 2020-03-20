a75449388dae93691145ca50d247bf39e9e9204f
haw <haw358linux@163.com>
2020-03-20 17:56:33 +0800

    封装存取cookie操作

--- src/main/java/com/haw/shop/util/Utils.java
+++ src/main/java/com/haw/shop/util/Utils.java
@@ -5,22 +5,77 @@ import com.haw.shop.service.UserService;
 import com.haw.shop.vo.UserInfoVo;
 import org.springframework.beans.BeanUtils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by aiwei on 2020-3-19.
  */
 public class Utils {
-    public static UserInfoVo buildUserInfoVo(Integer userId, UserService userService){
+    public static UserInfoVo buildUserInfoVo(Integer userId, UserService userService) {
         UserInfoVo userInfoVo = new UserInfoVo();
-        if(userId!=null){
+        if (userId != null) {
             UserInfo userInfo = userService.getUser(userId);
             BeanUtils.copyProperties(userInfo, userInfoVo);
             userInfoVo.setIsLogin(true);
-        }else{
+        } else {
             userInfoVo.setIsLogin(false);
         }
         return userInfoVo;
+    }
+
+    /**
+     * 设置cookie
+     *
+     * @param response
+     * @param name     cookie名字
+     * @param value    cookie值
+     * @param maxAge   cookie生命周期  以秒为单位
+     */
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        if (maxAge > 0) {
+            cookie.setMaxAge(maxAge);
+        }
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 根据名字获取cookie
+     *
+     * @param request
+     * @param name    cookie名字
+     * @return
+     */
+    public static Cookie getCookieByName(HttpServletRequest request, String name) {
+        Map<String, Cookie> cookieMap = Utils.readCookieMap(request);
+        if (cookieMap.containsKey(name)) {
+            Cookie cookie = (Cookie) cookieMap.get(name);
+            return cookie;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 将cookie封装到Map里面
+     *
+     * @param request
+     * @return
+     */
+    private static Map<String, Cookie> readCookieMap(HttpServletRequest request) {
+        Map<String, Cookie> cookieMap = new HashMap<String, Cookie>();
+        Cookie[] cookies = request.getCookies();
+        if (null != cookies) {
+            for (Cookie cookie : cookies) {
+                cookieMap.put(cookie.getName(), cookie);
+            }
+        }
+        return cookieMap;
     }
 }
